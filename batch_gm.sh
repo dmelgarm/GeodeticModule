3dff46ae6244b3dafe1c14bd34cccb9bb866bc64
dmm <dmelgarm@ucsd.edu>
2013-10-11 14:15:48 -0700

    first commit

---
+++ batch_gm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Where are the GM codes?
+gmpath=/home/sopac/GM/src
+
+#Get the data from Caltech
+python ${gmpath}/getstp.py
---
+++ getstp.py
@@ -0,0 +1,61 @@
+import os, datetime, subprocess
+from numpy import genfromtxt
+
+#Day to download
+
+#Specific date
+#day=7
+#month=10
+#year=2013
+
+#Today's date
+now=datetime.date.today()-datetime.timedelta(days=1)
+day=now.day
+year=now.year
+month=now.month
+
+#Generate file structure
+dir='/home/sopac/GM/'
+stas=genfromtxt(dir+'info/stalist.txt',dtype='str')
+#What day of year are we working on
+doy=datetime.date(year,month,day).strftime('%j')
+#Make the directory
+dir=dir+'data/'+str(year)+doy
+try:
+    os.mkdir(dir)
+    os.chdir(dir)
+except:
+    pass
+    os.chdir(dir)
+    os.remove('stp_accel.log')
+    os.remove('stp_gps.log')
+
+#get data through STP
+for k in range(stas.size):
+    #Get accelerometer data
+    stp= 'win GD '+stas[k]+' HN_ '+str(year)+'/'+str(month)+'/'+str(day)+',00:00:00 +1d'
+    print 'STP '+stp
+    pdata=subprocess.Popen('AIST2stp',stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    s='''v
+%s
+quit''' % stp
+    out,err=pdata.communicate(s)
+    #Write log
+    f=open('stp_accel.log','a')
+    f.write(out)
+    f.write(err)
+    f.close()
+    #Get GPS data
+    stp= 'win GD '+stas[k]+' LY_ '+str(year)+'/'+str(month)+'/'+str(day)+',00:00:00 +1d'
+    print 'STP '+stp
+    pdata=subprocess.Popen('AISTstp',stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    s='''v
+%s
+quit''' % stp
+    out,err=pdata.communicate(s)
+    #Write log
+    f=open('stp_gps.log','a')
+    f.write(out)
+    f.write(err)
+    f.close()
+    
